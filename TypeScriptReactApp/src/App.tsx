4d688d644c2b44bb08bbf9d4488e3d2d9a578a73
sharmaine1028 <sharmaine1028@gmail.com>
2023-02-02 21:51:30 +0800

    Remove submodule

---
+++ TypeScriptReactApp/src/App.tsx
@@ -0,0 +1,16 @@
+import NewTodo from "./components/NewTodo";
+import Todos from "./components/Todos";
+import TodosContextProvider from "./store/todos-context";
+
+function App() {
+  return (
+    <div>
+      <TodosContextProvider>
+        <NewTodo />
+        <Todos />
+      </TodosContextProvider>
+    </div>
+  );
+}
+
+export default App;
